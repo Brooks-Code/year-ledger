62cede67f446b61c590e9257f00cbf26dcb3c23a
Brooks Cole <marinjyang@outlook.com>
2026-08-12 14:04:52 +0800

    1、添加小工具

---
+++ src/test/java/com/brooks/mall/user/functional/GadgetUtil.java
@@ -0,0 +1,29 @@
+package com.brooks.mall.user.functional;
+
+
+import java.util.Scanner;
+
+/**
+ * TODO
+ *
+ * @Author mr.yang
+ * @Date 2026/8/12 13:55
+ */
+public class GadgetUtil {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 获取 Scanner 对象
+     * 作用：获取用户输入
+     */
+    private static void getScanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入要执行的命令：\n");
+        int input = scanner.nextInt();
+        int output = scanner.nextInt();
+        int a = input + output;
+        System.out.print("执行结果："+ a);
+    }
+}
